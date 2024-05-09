9cb6870b8c212faad800e70ffb8db455076b859f
JacobKing4220 <165833652+JacobKing4220@users.noreply.github.com>
2024-05-09 12:47:51 -0700

    Add files via upload

---
+++ Classes/ProfClasses/ProfClassData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ProfessorClasses
+{
+    public class Professor   {
+
+
+
+        private string Lnum;
+        private string firstName;
+        private string lastName;
+        private string dept;
+
+        public Professor() { }
+
+        public Professor(string profLnum, string profFName, string profLName, string profDept)
+        {
+            Lnum = profLum;
+            firstName = profFName;
+            lastName = profLName;
+            dept = profDept;
+            
+        }
+
+
+        public string ProfLnum
+        {
+            get
+            {
+                return Lnum;
+            }
+            set
+            {
+                Lnum = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string Dept
+        {
+            get
+            {
+                return dept;
+            }
+            set
+            {
+                dept = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            // to change
+            return String.Format("Lnumber: {0} First Name: {1} Last Name: {2} Dept: {3} " id, firstName, lastName, dept);
+        }
+
+    }
+}
---
+++ Classes/ProfTest/ProfTester.cs
@@ -0,0 +1,22 @@
+﻿uusing System;
+using System.Diagnostics;
+using ProfessorClasses;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CustomerProductTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteProf();
+        }
+
+        static void WriteProf()
+        {
+            Professor p1 = new Professor("L", "Brad", "Bird", "Computer Science");
+            Console.WriteLine(p1.ToString());
+        }
+
+    }
+}
